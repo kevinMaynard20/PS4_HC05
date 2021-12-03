fb782d34fecdb53f109d8ce59d37e14043942dda
Kevin Maynard <kevinmaynard@Kevins-MacBook-Pro.local>
2021-12-02 20:37:45 -0500

    Updated 2021-2022 version

--- PS4-HC05-Main.py
+++ PS4-HC05-Main.py
@@ -12,11 +12,13 @@
 #
 # Distributed under terms of the MIT license
 import bluetooth
-import os #does not work for some reason
-#import subprocess #failed attempt to fix
+import os 
 import pprint
 import pygame
 import serial
+
+FILE_PATH = '/dev/tty.HC-05-SerialPort'
+
 
 class PS4Controller(object):
     """Class representing the PS4 controller. Pretty straightforward functionality."""
@@ -49,8 +51,8 @@ class PS4Controller(object):
             self.hat_data = {}
             for i in range(self.controller.get_numhats()):
                 self.hat_data[i] = (0, 0)
-
-        ser = serial.Serial('/dev/tty.HC-05-SerialPort')
+        
+        ser = serial.Serial(FILE_PATH)
 
 
 
@@ -61,61 +63,47 @@ class PS4Controller(object):
                         if event.axis == 0:
                             if event.value > 0:
                                 print("right")
-                                # os.system("echo -n -e '\xFE\x6C\x01' > NAME_OF_YOUR_CHIP")
                             if event.value < 0:
                                 print("left")
-                                # os.system("echo -n -e '\xFE\x6C\x01' > NAME_OF_YOUR_CHIP")
                             if event.axis == 1:
                                 if event.value > 0:
                                     print ("down")
-                                    # os.system("echo -n -e '\xFE\x6C\x01' > NAME_OF_YOUR_CHIP")
                                 if event.value < 0:
                                     print ("up")
-                                    # os.system("echo -n -e '\xFE\x6C\x01' > NAME_OF_YOUR_CHIP")
                     elif event.type == pygame.JOYBUTTONDOWN:
                         if event.button == 1:
                             print("1")
                             ser.write(b'z')
-                            #os.system("echo -n -e '\x7A' > /dev/tty.HC-05-SerialPort")
                         if event.button == 2:
                             print("2")
                             ser.write(b'Z')
-                            #os.system("echo -n -e '\x5A' > /dev/tty.HC-05-SerialPort")
                         if event.button == 3:
                             print("3")
                             ser.write(b'C')
-                            #os.system("echo -n -e '\x43' > /dev/tty.HC-05-SerialPort")
                         if event.button == 0:
                             print("0")
                             ser.write(b'A')
-                            #os.system("echo -n -e '\x41' > /dev/tty.HC-05-SerialPort")
                     elif event.type == pygame.JOYBUTTONUP:
                         if event.button == 1:
                             print("x release")
                             ser.write(b'C')
-                            #os.system("echo -n -e '\x43' > /dev/tty.HC-05-SerialPort")
                         if event.button == 2:
                             ser.write(b'C')
                             print("circle release")
-                            #os.system("echo -n -e '\x43' > /dev/tty.HC-05-SerialPort")
                     elif event.type == pygame.JOYHATMOTION:
                         if event.hat == 0:
                             if event.value == (1, 0):
                                 print("right")
                                 ser.write(b'c')
-                                #os.system("echo -n -e '\x63' > /dev/tty.HC-05-SerialPort")
                             if event.value == (-1, 0):
                                 print("left")
                                 ser.write(b'b')
-                                #subprocess.run(["sudo", "-i", "echo", "-n", "-e" "'\x62' > /dev/tty.HC-05-SerialPort"] , shell=True)
                             if event.value == (0, 1):
                                 print("up")
                                 ser.write(b'a')
-                                #subprocess.run(["echo -n -e '\x61' > /dev/tty.HC-05-SerialPort"], shell=True)
                             if event.value == (0, -1):
                                 print("down")
                                 ser.write(b'd')
-                                #os.system("echo -n -e '\x64' > /dev/tty.HC-05-SerialPort")
 
 
         os.system('clear')
